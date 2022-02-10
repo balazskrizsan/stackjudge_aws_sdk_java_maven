b7917a8f48f24877902ffa16fc4441bf11e6f6e8
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-02-10 21:29:33 +0100

    Switch back to java 11

--- src/main/java/com/kbalazsworks/stackjudge_aws_sdk/schema_parameter_objects/PostCompanyOwnEmailRequest.java
+++ src/main/java/com/kbalazsworks/stackjudge_aws_sdk/schema_parameter_objects/PostCompanyOwnEmailRequest.java
@@ -3,10 +3,54 @@ package com.kbalazsworks.stackjudge_aws_sdk.schema_parameter_objects;
 import javax.annotation.processing.Generated;
 
 @Generated("OpenSDK: https://github.com/balazskrizsan/OpenSdk")
-public record PostCompanyOwnEmailRequest(
-    String to,
-    String varName,
-    String varOwnUrl
+public final class PostCompanyOwnEmailRequest
+{
+    private final String to;
+    private final String varName;
+    private final String varOwnUrl;
 
-)
-{}
+    public PostCompanyOwnEmailRequest(
+        String to,
+        String varName,
+        String varOwnUrl
+
+    )
+    {
+        this.to        = to;
+        this.varName   = varName;
+        this.varOwnUrl = varOwnUrl;
+    }
+
+    public String to()
+    {
+        return to;
+    }
+
+    public String varName()
+    {
+        return varName;
+    }
+
+    public String varOwnUrl()
+    {
+        return varOwnUrl;
+    }
+
+    // not yet supported in generator
+//    @Override
+//    public boolean equals(Object obj)
+//    {
+//    }
+
+    // not yet supported in generator
+//    @Override
+//    public int hashCode()
+//    {
+//    }
+
+    // not yet supported in generator
+//    @Override
+//    public String toString()
+//    {
+//    }
+}
