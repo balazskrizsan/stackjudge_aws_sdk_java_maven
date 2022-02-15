90ac66e132f4dc02a5efea6f3e3001ac6ca19e82
Balazs Krizsan <krizsan.balazs@gmail.com>
2022-02-15 14:56:56 +0100

    Naming fixes #2

--- src/main/java/com/kbalazsworks/stackjudge_aws_sdk/schema_interfaces/IS3Upload.java
+++ src/main/java/com/kbalazsworks/stackjudge_aws_sdk/schema_interfaces/IS3Upload.java
@@ -1,5 +1,6 @@
 package com.kbalazsworks.stackjudge_aws_sdk.schema_interfaces;
 
+import com.kbalazsworks.stackjudge_aws_sdk.common.interfaces.IOpenSdkPostable;
 import com.kbalazsworks.stackjudge_aws_sdk.schema_parameter_objects.*;
 
 import javax.annotation.processing.Generated;
@@ -12,5 +13,5 @@ public interface IS3Upload
         return "/s3/upload";
     }
 
-    void execute(PostUploadRequest postUploadRequest);
+    void execute(IOpenSdkPostable postUploadRequest);
 }
